551123a534c39188ae477cbd8250c23414e2def1
Divyanshi Patil <divyanshipatil26@gmail.com>
2025-08-06 22:56:01 +0530

    Update client.py

--- project3/client.py
+++ project3/client.py
@@ -9,10 +9,11 @@ client = OpenAI(
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
+    {"role": "system", "content": "You are a virtual assistant named AURA skilled in general tasks like Alexa and Google Cloud"},
     {"role": "user", "content": "what is coding"}
   ]
 )
 
 print(completion.choices[0].message.content)
+
 
